b1f64a8e161d6e56bcfa613299f45226d62f967f
Leif Blake <the.leif.blake@gmail.com>
2025-03-28 12:16:48 -0700

    Fix signed integer conversion

--- src/universal_format.py
+++ src/universal_format.py
@@ -76,7 +76,6 @@ class UniversalFormat:
             self.value = int(dec_string)
         elif self.value_type == "signed":
             # Convert to signed integer
-            dec_string = dec_string.lstrip('-')
             self.value = int(dec_string)
         elif self.value_type == "floating":
             # Convert to floating point number
@@ -114,18 +113,6 @@ class UniversalFormat:
 
         return self.value
 
-    def _clean_string(self, string):
-        """
-        Clean the string by removing leading and trailing whitespace
-        :param string: Input string
-        :return: Cleaned string
-        """
-
-        if self.value_type is None:
-            raise ValueError("Type must be set before conversion")
-
-        return string.strip()
-
     def to_hex_string(self, pad=False, show_0x=False):
         """
         Convert the value to a hexadecimal string representation.
@@ -133,8 +120,20 @@ class UniversalFormat:
         :param show_0x:
         :return:
         """
-        # Convert to hexadecimal string
-        hex_string = hex(self.value)
+        self._check_value()
+
+        if self.value_type == "unsigned":
+            hex_string = hex(self.value)
+        elif self.value_type == "signed":
+            if self.value >= 0:
+                min_bits_signed = np.min(pad_bit_values[pad_bit_values >= np.log2((np.abs(self.value) + 1) * 2)])
+                self.min_bits = max(self.min_bits, min_bits_signed) if self.min_bits is not None else min_bits_signed
+                hex_string = hex(self.value)
+            else:
+                min_bits_signed = np.min(pad_bit_values[pad_bit_values >= np.log2(np.abs(self.value) * 2)])
+                hex_string = hex(2**min_bits_signed + self.value)
+        elif self.value_type == "floating":
+            hex_string = hex(int(self.value))
 
         # Remove '0x' and '-0x' prefixes
         if hex_string.startswith("-0x"):
@@ -142,7 +141,7 @@ class UniversalFormat:
         elif hex_string.startswith("0x"):
             hex_string = hex_string[2:]
 
-        pad_val = '0' if self.value >= 0 else 'F'
+        pad_val = '0' if self.value >= 0 else 'f'
 
         # Pad to minimum nibbles
         if self.min_bits is not None and self.min_bits > len(hex_string) * 4:
@@ -165,6 +164,7 @@ class UniversalFormat:
                 Convert the value to a decimal string representation.
                 :return:
                 """
+        self._check_value()
         decimal_string = str(self.value)
 
         return decimal_string
@@ -176,28 +176,64 @@ class UniversalFormat:
         :param pad:
         :return:
         """
-        binary_string = bin(self.value)
+        self._check_value()
+        if self.value_type == "unsigned":
+            bin_string = bin(self.value)
+        elif self.value_type == "signed":
+            if self.value >= 0:
+                min_bits_signed = np.min(pad_bit_values[pad_bit_values >= np.log2((np.abs(self.value) + 1) * 2)])
+                self.min_bits = max(self.min_bits, min_bits_signed) if self.min_bits is not None else min_bits_signed
+                bin_string = bin(self.value)
+            else:
+                min_bits_signed = np.min(pad_bit_values[pad_bit_values >= np.log2(np.abs(self.value) * 2)])
+                bin_string = bin(2**min_bits_signed + self.value)
+        elif self.value_type == "floating":
+            bin_string = bin(int(self.value))
 
         # Remove '0b' and '-0b' prefixes
-        if binary_string.startswith("-0b"):
-            binary_string = binary_string[3:]
-        elif binary_string.startswith("0b"):
-            binary_string = binary_string[2:]
+        if bin_string.startswith("-0b"):
+            bin_string = bin_string[3:]
+        elif bin_string.startswith("0b"):
+            bin_string = bin_string[2:]
 
         # Pad to minimum bits
         pad_val = '0' if self.value >= 0 else '1'
-        if self.min_bits is not None and self.min_bits > len(binary_string):
-            num_pad = self.min_bits - len(binary_string)
+        if self.min_bits is not None and self.min_bits > len(bin_string):
+            num_pad = self.min_bits - len(bin_string)
             # Pad with zeros or ones based on sign
-            binary_string = pad_val * num_pad + binary_string
+            bin_string = pad_val * num_pad + bin_string
 
         # Pad the string if desired
         if pad:
-            num_pad = np.min(pad_bit_values[pad_bit_values >= len(binary_string)]) - len(binary_string)
-            binary_string = pad_val * num_pad + binary_string
+            num_pad = np.min(pad_bit_values[pad_bit_values >= len(bin_string)]) - len(bin_string)
+            bin_string = pad_val * num_pad + bin_string
 
         # Add 0b prefix if desired
         if show_0b:
-            binary_string = "0b" + binary_string
+            bin_string = "0b" + bin_string
 
-        return binary_string
+        return bin_string
+
+    def _clean_string(self, string):
+        """
+        Clean the string by removing leading and trailing whitespace
+        :param string: Input string
+        :return: Cleaned string
+        """
+
+        if self.value_type is None:
+            raise ValueError("Type must be set before conversion")
+
+        return string.strip()
+
+    def _check_value(self):
+        """
+        Check if the value is set
+        :return:
+        """
+        if self.value is None:
+            raise ValueError("Value must be set before conversion")
+        if self.value_type is None:
+            raise ValueError("Type must be set before conversion")
+
+        return True
